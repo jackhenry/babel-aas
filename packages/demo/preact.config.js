a8ab9adb394585571913eef631133822caa75986
Jack Henry <jack@jackerickson.net>
2022-03-30 23:57:08 -0500

    Changed public path to accomodate github pages.

--- packages/demo/preact.config.js
+++ packages/demo/preact.config.js
@@ -1,6 +1,7 @@
 export default (config, env, helpers) => {
-  const service_url = env.isProd ? 'http://34.71.120.215' : 'http://localhost:9001'
+  config.output.publicPath = env.isProd ? '/babel-aas/' : '/';
+  const service_url = env.isProd ? 'http://34.71.120.215' : 'http://localhost:9001';
   const definePlugin = helpers.getPluginsByName(config, 'DefinePlugin')[0].plugin;
   definePlugin.definitions['process.env.BAAS_SERVICE_URL'] = JSON.stringify(service_url);
   return config;
-}
+};
