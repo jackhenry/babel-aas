9067f105c0c5191196a593b37ef743e9ef97b543
Jack Henry <jack@jackerickson.net>
2022-04-02 16:46:34 -0500

    New pages build.

--- packages/demo/preact.config.js
+++ packages/demo/preact.config.js
@@ -1,6 +1,6 @@
 export default (config, env, helpers) => {
   config.output.publicPath = env.isProd ? '/babel-aas/' : '/';
-  const service_url = env.isProd ? 'https://34.71.120.215' : 'http://localhost:9001';
+  const service_url = env.isProd ? 'https://baas-gateway-pjrrdkdfpa-uc.a.run.app' : 'http://localhost:9001';
   const definePlugin = helpers.getPluginsByName(config, 'DefinePlugin')[0].plugin;
   definePlugin.definitions['process.env.BAAS_SERVICE_URL'] = JSON.stringify(service_url);
   return config;
