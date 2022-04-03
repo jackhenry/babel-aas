3d2c13a784215817a0db6d68b959d8c178674c93
Jack Henry <jack@jackerickson.net>
2022-04-03 18:28:58 -0500

    Removed floating controls.

--- packages/demo/src/views/editors/index.tsx
+++ packages/demo/src/views/editors/index.tsx
@@ -1,21 +1,15 @@
 import { FunctionalComponent, h } from 'preact'
 import Editor from '../../components/editor';
-import FloatingControlsView from '../floating';
 import ParametersView from '../parameters';
 import './editors.css'
 
 const EditorsView: FunctionalComponent = () => {
-  // Since this a view, I'm going to add reducer logic here because it applies to UI
-
-
-
   return (
     <div id="ui-container" class='h-full flex md:overflow-hidden'>
       <ParametersView />
       <div id="editors-controls-wrapper" class="w-full flex relative">
         <Editor isInput={true} />
         <Editor isInput={false} />
-        <FloatingControlsView />
       </div>
     </div>
   )
