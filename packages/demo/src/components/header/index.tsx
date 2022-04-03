96901562ef87d94fb6493f7cfe9474029134b0f9
Jack Henry <jack@jackerickson.net>
2022-04-03 18:29:51 -0500

    Changed layout of submission controls and parameters box.

--- packages/demo/src/components/header/index.tsx
+++ packages/demo/src/components/header/index.tsx
@@ -1,8 +1,23 @@
 import { FunctionalComponent, h } from "preact";
+import Backend from "../../helpers/backend";
+import { useAppDispatch, useAppSelector } from "../../hooks";
+import { selectShowDrawer, toggle } from "../../reducers/uiSlice";
 import GithubIcon from "../common/github";
 import SunIcon from "../common/sun";
 
 const Header: FunctionalComponent = () => {
+  
+  const isDrawerShown = useAppSelector(selectShowDrawer);
+  const dispatch = useAppDispatch();
+
+  const handleDrawerToggle = () => {
+    dispatch(toggle(!isDrawerShown));
+  }
+
+  const handleSend = () => {
+    // The code works. The types need to be fixed though so tsc stops complaining.
+    dispatch(Backend.compileRequest);
+  }
 
   return (
     <div class="flex px-2 py-1.5 bg-override-bg">
@@ -11,9 +26,21 @@ const Header: FunctionalComponent = () => {
           <SunIcon classes="!fill-override-blue animate-spin-slow" />
           <span class="ml-2 text-override-blue font-bold place-self-start">baas demo</span>
         </div>
-        <a href="https://www.jackhenry.io#projects">
-          <GithubIcon classes="!fill-override-fg/75 hover:!fill-override-blue w-auto h-6" />
-        </a>
+        <div class="flex justify-center items-center gap-x-4">
+          <button class="flex grow justify-center" onClick={handleSend}>
+            <span class="material-icons text-xl text-override-green hover:text-override-green/50">
+              send
+            </span>
+          </button>
+          <button class="flex grow justify-center" onClick={handleDrawerToggle}>
+            <span class="material-icons text-xl text-override-yellow hover:text-override-yellow/50">
+              settings
+            </span>
+          </button>
+          <a href="https://www.jackhenry.io#projects">
+            <GithubIcon classes="!fill-override-fg/75 hover:!fill-override-blue w-auto h-6" />
+          </a>
+        </div>
       </div>
     </div>
   )
